2d1e7a3676e98eb3f8f7ddc99967f9e764baece4
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2024-06-27 14:47:29 +0900

    메인페이지 html 작성 / header css 작업(nav hover 애니메이션 진행)

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const swiper1 = new Swiper('.store-slide',{
+    effect:'',
+    autoplay:{},
+    //cube 옵션 : effect:'cube' 설정 시 가능
+    //자동재생(기본 3초(3000))
+    //autoplay:{delay:2000},
+    //마지막 슬라이드에서 시작 슬라이드로 자연스럽게 변환
+    loop:true,//기본값이 false임
+});
