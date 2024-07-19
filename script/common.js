bfaa831ef2a1f6b0956a68c5310701b810d88d70
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2024-07-19 18:04:13 +0900

    모바일 사이드바 제작

--- script/common.js
+++ script/common.js
@@ -16,6 +16,7 @@ const swiper4 = new Swiper('.rec-slide',{
 const search_icn = document.querySelector('header .lnb > .search')
 const search_container = document.querySelector('header .search_container')
 const close = document.querySelector('header .search_container .close')
+
 /* 검색창 초기값 */
 search_container.style.transform = 'translateY(-100vh)'
 search_icn.addEventListener('click',(e)=>{
@@ -35,4 +36,22 @@ const lang_opt = document.querySelector('.lang_opt')
 lang.addEventListener('click',(e)=>{
     e.preventDefault();
     lang_opt.classList.toggle('on')
+})
+
+//m_nav 모바일 전용 메뉴
+const nav = document.querySelector('nav')
+const nav_clone = nav.cloneNode(true) //nav 복제 변수
+const m_nav = document.querySelector('.m_nav')
+const m_nav_wrap = document.querySelector('.m_nav_wrap')
+const m_nav_lnb = document.querySelector('.m_nav_lnb')
+const all_nav = document.querySelector('.all_nav')
+
+m_nav_lnb.appendChild(nav_clone)
+console.log(m_nav_lnb.children)
+
+m_nav_wrap.style.display = 'none'
+all_nav.addEventListener('click',(e)=>{
+    e.preventDefault();
+    m_nav_wrap.style.display = 'block'
+    all_nav.style.display = 'none'
 })
