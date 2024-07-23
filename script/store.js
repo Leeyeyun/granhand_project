3c19c3ff22c418989ad18223c5b021092d6d73ac
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2024-07-23 11:02:22 +0900

    store 반응형 페이지 작업

--- script/store.js
+++ script/store.js
@@ -1,13 +1,6 @@
+//granhand & komfortabel 카테고리 별 매장 이름 나오기
 const type = document.querySelectorAll('.store_type li .type')
-console.log(type);
-console.log(type[1].nextElementSibling);
-/* for(let i of type){
-    i.addEventListener('click',(e)=>{
-        e.preventDefault();
-        i.style.borderBottom = '2px solid #000';
-        i.style.color = '#000';
-    })
-} */
+
 type[0].addEventListener('click',(e)=>{
     e.preventDefault();
     type[0].style.borderBottom = '2px solid #000';
@@ -25,4 +18,16 @@ type[1].addEventListener('click',(e)=>{
     type[0].style.borderBottom = '0';
     type[0].style.color = '#9d9d9d';
     type[0].nextElementSibling.style.display = 'none';
+})
+
+//scroll trigger
+//gsap.to('적용대상',{scrollTrigger:'뷰포트인식대상'})
+gsap.to('.s2 img',{
+    scrollTrigger:'.s2',
+    duration:1,//1초
+    delay:0.2,
+    y:100,
+    borderRadius:50,
+    scale:0.8,
+    opacity:1,
 })
